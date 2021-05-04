b24eaa3155e6c6bfb1df7798340870f91043a02d
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-05-04 14:18:09 -0400

    awesome slides

--- fash-un/src/pages/Home.js
+++ fash-un/src/pages/Home.js
@@ -6,13 +6,13 @@ const Home = () => {
         <AwesomeSlider style={{height:"500px", margin:"100px auto"}}
         media={[
           {
-            source: 'https://i.ibb.co/GQMdmMj/home.png',
-          },
-          {
-            source: 'https://assets.vogue.com/photos/6012d4685312e25a9789e6f5/master/pass/00003-JUNYA-WATANABE-MENSWEAR-FALL-21.jpg',
+            source: 'https://i.ibb.co/L1MMysP/Untitled-2.png',
           },
           {
             source: 'https://assets.vogue.com/photos/60112788d056dd15bb8731e3/master/pass/00003-COMME-DES-GARCONS-HOMME-PLUS-FALL-21.jpg',
+          },
+          {
+            source: 'https://i.ibb.co/JqFPkMg/Untitled-3.png',
           },
         ]}
       />
